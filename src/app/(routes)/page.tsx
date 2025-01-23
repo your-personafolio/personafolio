74e22c59be4d7a69b805224cda77f33273396e94
your-personafolio <your.personafolio@gmail.com>
2025-01-23 22:09:37 +0700

    Update page.tsx

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,15 +1,10 @@
-import getBillboards from "@/features/billboards/core/services/api";
+
 import Home from "@/features/home/components";
-import { getProducts } from "@/features/product/core/services/api";
+
 
 export const revalidate = 0;
 
-const HomePage = async () => {
-  const products = await getProducts({
-    isFeatured: true,
-  });
-  const billboard = await getBillboards();
-
+const HomePage = async () => { 
   return <Home />;
 };
 
