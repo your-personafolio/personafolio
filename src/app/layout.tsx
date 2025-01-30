2ad2650399222d1a7ec46eff9c0aaa275eb7b537
your-personafolio <your.personafolio@gmail.com>
2025-01-31 00:33:55 +0700

    update: add google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
             {children}
             <Footer />
           </ThemeProvider>
-          <GoogleTagManager gtmId="	G-B2V8XHZ0PV" />
+          <GoogleTagManager gtmId="G-B2V8XHZ0PV" />
         </body>
       </html>
     </ClerkProvider>
