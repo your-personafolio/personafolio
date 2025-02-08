2a656870c83d7a2abcd556726994501a913806c9
your-personafolio <your.personafolio@gmail.com>
2025-02-08 17:07:57 +0700

    update: (hapus beberapa teks) untuk trigger vercel ubah ke branch staging

--- src/features/home/components/index.tsx
+++ src/features/home/components/index.tsx
@@ -161,7 +161,7 @@ const Home = ({ products }: IHomeProps) => {
             <div className="max-w-5xl px-4 xl:px-0 py-10 lg:pt-20 lg:pb-20 mx-auto">
               <div className="max-w-3xl mb-10 lg:mb-14">
                 <h2 className="text-white font-semibold text-2xl md:text-4xl md:leading-tight">
-                  Our approach
+                  Our
                 </h2>
                 <p className="mt-1 text-neutral-400">
                   This profound insight guides our comprehensive strategy — from
